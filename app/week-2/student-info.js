b46a738fe5161056825615dac8146fe253304aac
JAGDEEP-BARYAR <baryarjagdeepkaur82@gmail.com>
2024-01-17 21:20:18 -0700

    Complete week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+export default function StudentInfo() {
+  return (
+    <main>
+        <div>
+            <h1>Shopping List</h1>
+            <p><h2>Jagdeep Baryar</h2></p>
+            <h2><Link href="https://github.com">https://github.com</Link></h2>
+            </div>
+    </main>
+   
+  );
+}
+ 
