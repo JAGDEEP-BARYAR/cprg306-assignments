abe56a146ffba99b7d97fe1debb1151699c27f34
JAGDEEP-BARYAR <baryarjagdeepkaur82@gmail.com>
2024-01-25 13:16:34 -0700

    Update week-3 assignment

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ export default function Page() {
     <ul>
       <li>
         <h2><Link href="week-2">Week 2 Assignment</Link></h2>
+        <h2><Link href="week-3">Week 3 Assignment</Link></h2>
       </li>
     </ul>
     </main>
